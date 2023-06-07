f6b8a0a9907f2adcec55fa5ca061da34d0b94102
Arvid Ljungberg <arvid.lju@gmail.com>
2023-06-07 20:30:45 +0200

    Create navbar and footer for react page

---
+++ Client/squidclient/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  Nav,
+  NavLink,
+  Bars,
+  NavMenu,
+  NavBtn,
+  NavBtnLink
+} from './NavbarElements';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavLink to='/'>
+          <img src="../../images/logo.svg" alt="logo" />
+        </NavLink>
+        <Bars />
+        <NavMenu>
+          <NavLink to='/' activeStyle>
+            Home
+          </NavLink>
+          <NavLink to='/contact-us' activeStyle>
+            Contact Us
+          </NavLink>
+          <NavLink to='/sign-up' activeStyle>
+            Sign Up
+          </NavLink>
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to='/signin'>Sign In</NavBtnLink>
+        </NavBtn>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
